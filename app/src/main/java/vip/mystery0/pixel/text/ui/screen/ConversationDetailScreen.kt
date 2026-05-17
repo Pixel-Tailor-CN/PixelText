056d68f766d570f3fd410c95c57778a0926e7613
Mystery0 <mystery0dyl520@gmail.com>
2026-05-17 19:25:25 +0800

    修复软键盘遮挡发送栏

--- app/src/main/java/vip/mystery0/pixel/text/ui/screen/ConversationDetailScreen.kt
+++ app/src/main/java/vip/mystery0/pixel/text/ui/screen/ConversationDetailScreen.kt
@@ -19,9 +19,11 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsBottomHeight
 import androidx.compose.foundation.lazy.LazyColumn
@@ -320,7 +322,11 @@ fun ConversationDetailScreen(
                         }
                     }
                 }
-                Spacer(modifier = Modifier.windowInsetsBottomHeight(WindowInsets.navigationBars))
+                Spacer(
+                    modifier = Modifier.windowInsetsBottomHeight(
+                        WindowInsets.navigationBars.union(WindowInsets.ime)
+                    )
+                )
             }
         },
         containerColor = MaterialTheme.colorScheme.background
